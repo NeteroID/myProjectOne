c73ec301a008817b1746133525a0d0fa3ba5511c
NeteroID <neteroif@gmail.com>
2020-11-26 16:14:27 +0300

    second commit

--- pages/js/index.js
+++ pages/js/index.js
@@ -5,32 +5,38 @@ let currentYear = newDate.getFullYear()
 let currentMonth = newDate.getMonth()
 
 const getDayName = (year, month, day) => {
-    const date = new Date(year, month, day)
-    const options = {weekday: 'short'}
+    let date = new Date(year, month, day)
+    let options = {weekday: 'short'}
     return new Intl.DateTimeFormat("ru-RU", options).format(date)
 
 }
 
 const getFullDateName = (year, month, day) => {
-    const date = new Date(year, month, day)
-    const options = {year: 'numeric', month: 'short', day: 'numeric'}
+    let date = new Date(year, month, day)
+    let options = {year: 'numeric', month: 'short', day: 'numeric'}
     return new Intl.DateTimeFormat("ru-RU", options).format(date)
 
 }
 
 const getLastDayOfMonth = (year, month) => {
-    const date = new Date(year, month+1, 0)
+    let date = new Date(year, month+1, 0)
     return date.getDate()
 }
 
 function renderCalendar(LastDay) {
-    const periodPTag = document.getElementById('selected-period')
+    let periodPTag = document.getElementById('selected-period')
     periodPTag.innerText = getFullDateName(currentYear, currentMonth, 1) + ' - ' +
         getFullDateName(currentYear, currentMonth, LastDay)
 
-    const table = document.querySelector("#thiz-table")
+    let table = document.querySelector("#thiz-table")
     table.innerHTML = ''
+
     tr = document.createElement('tr')
+
+    td = document.createElement('td')
+    td.innerHTML = 'Object/Date'
+
+    tr.appendChild(td)
 
     for (let day = 1; day <= LastDay; day++){
         dayName = getDayName(currentYear, currentMonth, day)
@@ -41,6 +47,20 @@ function renderCalendar(LastDay) {
         tr.appendChild(td)
     }
     table.appendChild(tr)
+
+    objectLease.forEach(object => {
+        tr = document.createElement('tr')
+        td = document.createElement('td')
+        td.innerHTML = object
+        tr.appendChild(td)
+
+        for (let day = 1; day <= LastDay; day++){
+            td = document.createElement('td')
+            td.innerHTML = ''
+            tr.appendChild(td)
+        }
+        table.appendChild(tr)
+    })
 
 }
 
